9999cc91e069799f644e172fbc7856a2ad1ace6c
Marcos Calero <mcaler01@ucm.es>
2019-04-19 18:21:11 +0200

    Pipe en thread. No tiene mucho sentido pero para probar, hacer fork con el socket y pasarla.

--- main.c
+++ main.c
@@ -11,6 +11,9 @@
 #define MAX_FILES_IN_QUEUE 5
 #define MAX_FILENAME_LENGTH 150
 
+#define READ 0
+#define WRITE 1
+
 void *get_file_to_fix(void *arg);
 
 int fix_name ();
@@ -18,6 +21,10 @@ int search_metadata();
 
 void *rename_by_metadata (void *arg);
 void *rename_by_name (void *arg);
+void *test_pipe (void *arg);
+
+int pipe_name[2];
+int pipe_metadata[2];
 
 pthread_mutex_t mutex_metadata;
 pthread_mutex_t mutex_name;
@@ -35,10 +42,20 @@ int nQueue_metadata = 0;
 
 int main (int argc, char *argv[]){
   
-  pthread_t th1, th2, th3;
+  pthread_t th1, th2, th3, th4;
   
   if(chdir(work_directory)){
     perror("Chdir main");
+    exit(errno);
+  }
+  
+  if(pipe(pipe_name) == -1){
+    perror("Pipe name error");
+    exit(errno);
+  }
+  
+  if(pipe(pipe_metadata) == -1){
+    perror("Pipe metadata error");
     exit(errno);
   }
   
@@ -47,13 +64,17 @@ int main (int argc, char *argv[]){
   pthread_cond_init (&queue_cond_name, NULL);
   pthread_cond_init (&queue_cond_metadata, NULL);
   
-  pthread_create (&th1, NULL, rename_by_name, NULL);
+  //pthread_create (&th1, NULL, rename_by_name, NULL);
   pthread_create (&th2, NULL, rename_by_metadata, NULL);
   pthread_create (&th3, NULL, get_file_to_fix, NULL);
+  pthread_create (&th4, NULL, test_pipe, NULL);
   
-  pthread_join (th1, NULL);
+  
+ // pthread_join (th1, NULL);
   pthread_join (th2, NULL);
   pthread_join (th3, NULL);
+  pthread_join (th4, NULL);
+
 
   pthread_mutex_destroy(&mutex_metadata);
   pthread_mutex_destroy(&mutex_name);
@@ -77,6 +98,8 @@ void *get_file_to_fix(void *arg){
     
     while ((dp = readdir(dir)) != NULL) {
       if ((strcmp(dp->d_name, ".") != 0) && (strcmp(dp->d_name, "..") != 0)) {
+	
+	sleep(1);
 	
 	if (!strncmp(dp->d_name,"3",1)) {
 	  pthread_mutex_lock (&mutex_name);
@@ -143,6 +166,12 @@ void *rename_by_name (void *arg) {
 void *rename_by_metadata (void *arg) {
   
   char filename[MAX_FILENAME_LENGTH];
+  int result = 0;
+  /*
+  if (close(pipe_metadata[READ]) != 0){
+    perror("Error close pipe_metadata READ rename_by_metadata");
+    exit(errno);
+  }*/
   
   while(1){
     
@@ -151,38 +180,104 @@ void *rename_by_metadata (void *arg) {
     while (nQueue_metadata == 0) {
       pthread_cond_wait (&queue_cond_metadata, &mutex_metadata);
     }
-   
-    strcpy(filename, queue_fix_by_name[nQueue_metadata - 1]);
+    
+    strcpy(filename, queue_fix_by_metadata[nQueue_metadata - 1]);
     nQueue_metadata--;
-  
+    
     pthread_mutex_unlock (&mutex_metadata);
     
     if(search_metadata()){
-      //ok
+      //printf("ok\n");
     }
     else {
-      //pipe to socket
+      
+      printf("%lu: envio datos: %s\n",(unsigned long)time(NULL),filename);
+      printf("strlen(filename) %u\n",(unsigned int) strlen(filename));
+      
+      result = write(pipe_metadata[WRITE], filename, strlen(filename) + 1);
+      
+      if(result != strlen(filename) + 1){
+	perror("Error write pipe_metadata WRITE rename_by_metadata");
+	exit(errno);
+      }
+      
+//       if (close(pipe_metadata[WRITE]) != 0){
+// 	perror("Error close pipe_metadata WRITE rename_by_metadata");
+// 	exit(errno);
+//       }
+      
     }
-    
   }
   
   return NULL;
 }
 
-int search_metadata() {
+
+void *test_pipe (void *arg) {
   
-  fprintf(stdout, "%lu: search_metadata 2 SEC \n",(unsigned long)time(NULL));
-  sleep (2);
-  fprintf(stdout, "%lu: search_metadata FIN \n", (unsigned long)time(NULL));
- 
-  return 0;
+  //   sleep(1);
+  
+  int result = 0;
+  char received[MAX_FILENAME_LENGTH];
+  /*
+  if (close(pipe_metadata[WRITE]) != 0){
+        perror("Error close pipe_metadata WRITE on test_pipe");
+      exit(errno);
+  }*/
+  
+  while(1){
+    
+    bzero(received,MAX_FILENAME_LENGTH);
+    
+    result = read(pipe_metadata[READ],received, sizeof(received));
+    
+    if (result < 0) {
+      perror("Read pipe metadata READ on test_pipe");
+      exit(errno);
+    }
+    else {
+      printf("%lu: Received: %s\n",(unsigned long)time(NULL),received);
+      sleep(2);
+      printf("recibo\n");
+      //       if (close(pipe_metadata[READ]) != 0){
+	// 	perror("Error close pipe_metadata READ on test_pipe");
+      // 	exit(errno);
+      //}
+    }
+  }
+  
+  printf("termino");
+  
+  return NULL; 
   
 }
 
-int fix_name() {
+
+int search_metadata() {
   
+   int ret = 0;
+  
+  fprintf(stdout, "%lu: search_metadata 2 SEC \n",(unsigned long)time(NULL));
+   sleep (1); 
+  int dato = rand () % 99;
+    if (dato > 49)
+    {
+       ret = 0;
+    }
+    else
+    {
+      ret = 1;
+    }
+  sleep (1); 
+  fprintf(stdout, "%lu: search_metadata FIN \n", (unsigned long)time(NULL));
+
+  return ret;
+}
+
+int fix_name() {
+ 
   fprintf(stdout, "%lu: search_name 2 SEC \n",(unsigned long)time(NULL));
-  sleep (2);
+  sleep(2);
   fprintf(stdout, "%lu: search_name FIN \n", (unsigned long)time(NULL));
  
   return 0;
